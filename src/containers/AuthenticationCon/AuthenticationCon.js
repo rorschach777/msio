49d5a62443452f05081801520048baef3c74ca81
rorschach777 <msweitzer1987@gmail.com>
2019-09-25 00:34:14 -0400

    express updates

--- src/containers/AuthenticationCon/AuthenticationCon.js
+++ src/containers/AuthenticationCon/AuthenticationCon.js
@@ -26,7 +26,7 @@ class AuthenticationCon extends Component {
             text: 'Here is some text.'
         }
   
-        fetch(`http://127.0.0.1:3000/login?recipient=${email.recipient}&firstName=${email.firstName}&companyName=${email.companyName}&sender=${email.sender}&topic=${email.subject}&html=${email.html}&text=${email.text}&key=${this.props.rdxAuthState.sgKey}`) //query string url
+        fetch(`http://www.marksweitzer.io:3000/login?recipient=${email.recipient}&firstName=${email.firstName}&companyName=${email.companyName}&sender=${email.sender}&topic=${email.subject}&html=${email.html}&text=${email.text}&key=${this.props.rdxAuthState.sgKey}`) //query string url
             .catch(err => console.error(err))
     }
     componentDidMount(){
